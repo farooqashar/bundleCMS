93c728f9bf0c88a7a3eb5ffc0fe314135fab5bed
Ashar Farooq <ashar744@gmail.com>
2021-06-09 14:31:18 -0400

    Update BundleInfo.jsx

--- pages/admin/components/BundleInfo.jsx
+++ pages/admin/components/BundleInfo.jsx
@@ -1,29 +1,39 @@
-/* -----------------Child Components--------------- */
-import CustomAutocomplete from "..//components/CustomAutocomplete.jsx";
-import CustomTextField from "..//components/textField.jsx";
-import Header from "..//components/header.jsx";
-import Footer from "..//components/footer.jsx";
+// @flow
 
-/* -----------------Yup--------------- */
-import * as Yup from "yup";
+/* -----------------Globals--------------- */
+import React, { useState } from 'react';
+
+/* -----------------Child Components--------------- */
+import CustomMultiSelect from './CustomMultiSelect';
+import CustomTextField from './textField';
+import Header from './header';
+import AlertDialog from '../../common/dialogs/AlertDialog';
+import ImagePickerDialog from '../../create/ImagePickerDialog';
 
 /* -----------------Formik--------------- */
-import { Formik, Form } from "formik";
+import { Formik, Form } from 'formik';
 
 /* -----------------Nextjs--------------- */
-import Link from "next/link";
-import Head from "next/head";
+import Head from 'next/head';
 
 /* -----------------MaterialUI--------------- */
 import {
-  makeStyles,
-  Button,
   Container,
   Grid,
-  Typography,
-  IconButton, Box, Paper, Card, CardHeader, withStyles
-} from "@material-ui/core";
-import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+  Box,
+  Paper,
+  Card,
+  CardHeader,
+  withStyles,
+  ListSubheader,
+  List,
+  ListItem,
+  TextField,
+  Chip,
+  Button,
+} from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { CloudUpload } from '@material-ui/icons';
 
 /* -----------------Utils--------------- */
 import {
@@ -32,166 +42,291 @@ import {
   getFeatures,
   getAllCategories,
   getAllPublishers,
-  getNextBundles
-} from "..//utils.jsx";
+  getNextBundles,
+  bundleInfoformRequiredness,
+} from './utils';
 
 /* -----------------Styles--------------- */
-const styles = () => ({
+const styles = theme => ({
   formMargins: {
     marginTop: 4,
-    marginBottom: 4
-  }
+    marginBottom: 4,
+  },
+  container: {
+    paddingBottom: theme.spacing(12),
+  },
+  errorText: {
+    color: theme.palette.error.main,
+  },
+  Card: {
+    paddingBottom: 10,
+  },
 });
 
+/* -----------------Types--------------- */
+type Props = {
+  prepopulated: *,
+  optionsTitle: string,
+  title: string,
+  cancelLink: string,
+  discardLink: string,
+  breadcrumbs: [],
+  link: string,
+  dialogTitle: string,
+  cancelTitle: string,
+  discardTitle: string,
+  variant: string,
+  id: string
+}
+
 /* -----------------Component--------------- */
-const BundleInfo = ({
-  studyContent,
-  testimonials,
-  nextBundles,
-  features,
-  publishers,
-  categories,
-  id,
-  bundles,
-  prepopulated, 
-  optionsTitle, 
-  title
-}) => {
+const BundleInfo = (props: Props) => {
+  const {
+    prepopulated,
+    optionsTitle,
+    title,
+    breadcrumbs,
+    link,
+    id,
+  } = props;
 
+  const [name, setName] = useState('');
+  const [categories, setCategories] = useState(getAllCategories());
+  const [features, setFeatures] = useState(getFeatures());
+  const [publishers] = useState(getAllPublishers());
 
-  const formRequiredness = Yup.object().shape({
-    name: Yup.string().required("Required"),
-    tagline: Yup.string().required("Required"),
-    description: Yup.string().required("Required")
-  });
+  const [dialogVisible, setDialogVisible] = useState(false);
+  const [imagePickerVisibility, setImagePickerVisibility] = useState(false);
+
+  const onChangeTags = (event) => {
+    const updatedValues = [...categories];
+
+    if (event.target.value === undefined) {
+      console.log();
+    } else {
+      updatedValues.push({
+        name: event.target.value,
+        // Random Unique ID is needed
+      });
+      setCategories(updatedValues);
+    }
+  };
+
+  const onChangeFeatureTags = (event) => {
+    const updatedValues = [...features];
+
+    if (event.target.value === undefined) {
+      console.log();
+    } else {
+      updatedValues.push({
+        name: event.target.value,
+        // Random Unique ID is needed
+      });
+      setFeatures(updatedValues);
+    }
+  };
 
   return (
     <>
-
       <Head>
         <title>{optionsTitle}</title>
       </Head>
 
-
       <Container maxWidth="md">
-      <Header breadcrumbs={[
-          {
-            path: "/admin",
-            name: "Admin page"
-          },
-          {
-            path: "/admin/bundles",
-            name: "Bundles"
-          },
-          {
-            path: `/admin/bundles/${id}`,
-            name: title
-          },
-          {
-            path: `/admin/bundles/${id}/info`,
-            name: "Info"
-          }
-        ]} variant={"Save Bundle Info"} />
+        <Header
+          breadcrumbs={breadcrumbs}
+          variant="Save"
+          link={link}
+        />
+        <Box m={2} mb={6}>
+          <Card>
+            <CardHeader title={`Bundle ${id}`} />
+            <Paper>
+              <Grid container>
+                <Grid item xs={12}>
+                  <Box m={2} mb={2}>
+                    <Formik
+                      initialValues={{
+                        ...prepopulated,
+                      }}
+                      validationSchema={bundleInfoformRequiredness}
+                    >
+                      <Form>
 
-<Card>
-  <CardHeader title = {"Basic Information"}>
-  </CardHeader>
-</Card>
+                        <List>
 
-<Paper>
-      <Grid container>
-        <Grid item xs={12}>
+                          <Grid container spacing={2} direction="column">
+                            <ListSubheader>Data</ListSubheader>
+                            <ListItem>
+                              <Grid item xs={12}>
+                                <TextField
+                                  placeholder="e.g. USMLE Step 1 Flashcards"
+                                  defaultValue={name}
+                                  label="Name"
+                                  onChange={(e) => setName(e.target.value)}
+                                  variant="outlined"
+                                  fullWidth
+                                />
+                              </Grid>
+                            </ListItem>
 
-            <Box m={1.5}>              
-              <Formik
-                initialValues={{
-                  ...prepopulated
-                }}
-                validationSchema={formRequiredness}
-              >
-                <Form>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                      <CustomTextField placeholder = "i.e. USMLE Step 1 Flashcards" name="name" label="Name" />
-                    </Grid>
+                            <ListItem>
+                              <Grid item xs={12}>
+                                <CustomTextField
+                                  placeholder="i.e. You’ll find hundreds of questions that cover the content you’ll encounter on the actual shelf exam. The range of question topics covers the clerkship's core competencies and each question includes high yield explanations of both correct and incorrect answers."
+                                  name="description"
+                                  label="Description"
+                                  multiline
+                                  rows={4}
+                                />
+                              </Grid>
+                            </ListItem>
 
-                    <Grid item xs={12}>
-                      <CustomAutocomplete
-                        options={getAllPublishers()}
-                        label="Publishers"
-                      ></CustomAutocomplete>
-                    </Grid>
+                            <ListItem>
 
-                    <Grid item xs={12}>
-                      <CustomAutocomplete
-                        name = "categories"
-                        options={getAllCategories()}
-                        label="Categories"
-                      ></CustomAutocomplete>
-                    </Grid>
+                              <Grid item xs={12}>
+                                <CustomTextField
+                                  placeholder="i.e.Your essential rapid-fire study companion and high-yield resource for the surgery shelf and clerkship"
+                                  name="tagline"
+                                  label="Tag Line"
+                                />
+                              </Grid>
+                            </ListItem>
 
-                    <Grid item xs={12}>
-                      <CustomAutocomplete
-                        options={getNextBundles()}
-                        label="Next Bundles"
-                      ></CustomAutocomplete>
-                    </Grid>
+                            <ListItem>
+                              <Grid item xs={12}>
+                                <Button
+                                  variant="contained"
+                                  onClick={() => setImagePickerVisibility(!imagePickerVisibility)}
+                                >
+                                  <CloudUpload />
+                                  Upload Logo
+                                </Button>
+                                <ImagePickerDialog
+                                  currentFactIndex={2}
+                                  currentTermIndex={1}
+                                  studySetId="studySetId"
+                                  contentItemId="contentItemId1"
+                                  itemType="New"
+                                  visible={imagePickerVisibility}
+                                  callback={() => setImagePickerVisibility(!imagePickerVisibility)}
+                                />
+                              </Grid>
+                            </ListItem>
+                            <ListItem>
+                              <Grid item xs={12}>
+                                <Autocomplete
+                                  multiple
+                                  fullWidth
+                                  id="features"
+                                  options={features.map((option) => option.name)}
+                                  freeSolo
+                                  renderTags={(tag, getTagProps) => tag.map((option, index) => (
+                                    <>
+                                      <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+                                    </>
+                                  ))
+        }
+                                  onChange={(event) => onChangeFeatureTags(event)}
+                                  renderInput={(params) => (
+                                    <TextField {...params} variant="outlined" label="Features" placeholder="Feature" />
+                                  )}
+                                />
+                              </Grid>
+                            </ListItem>
 
-                    <Grid item xs={12}>
-                      <CustomTextField
-                        placeholder = "i.e. You’ll find hundreds of questions that cover the content you’ll encounter on the actual shelf exam. The range of question topics covers the clerkship's core competencies and each question includes high yield explanations of both correct and incorrect answers."
-                        name="description"
-                        label="Description"
-                        multiline
-                        rows={4}
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <CustomTextField placeholder = "i.e.Your essential rapid-fire study companion and high-yield resource for the surgery shelf and clerkship" name="tagline" label="Tag Line" />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <CustomAutocomplete
-                        options={getFeatures()}
-                        label="Features"
-                      ></CustomAutocomplete>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <CustomAutocomplete
-                        options={getStudyContent()}
-                        label="Study Content"
-                      ></CustomAutocomplete>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <CustomAutocomplete
-                        options={getTestimonials()}
-                        label="Testimonials"
-                      ></CustomAutocomplete>
-                    </Grid>
+                            <ListItem>
+                              <Grid item xs={12}>
+                                <CustomMultiSelect
+                                  options={getTestimonials()}
+                                  label="Testimonials"
+                                  dialogLabel="Testimonial"
+                                  mutable
+                                />
+                              </Grid>
+                            </ListItem>
 
-                    <Grid item xs={12}>
-                      <b1>Upload Avatar</b1>
-                      <IconButton>
-                        <CloudUploadIcon />
-                      </IconButton>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <b1>Upload Logo</b1>
-                      <IconButton>
-                        <CloudUploadIcon />
-                      </IconButton>
-                    </Grid>
-                  </Grid>
-                </Form>
-              </Formik>
-              <Footer dialogTitle = {"Discard Changes?"} cancelTitle = {"Go Back"} discardTitle = {"Discard"} variant = {"Discard Changes"}></Footer>
-            </Box>
+                            <ListSubheader>Meta Data</ListSubheader>
+                            <ListItem>
+                              <Grid item xs={12}>
+                                <Autocomplete
+                                  id="publishers"
+                                  options={publishers}
+                                  getOptionLabel={(option) => option.name}
+                                  renderInput={(params) => <TextField {...params} label="Publisher" variant="outlined" />}
+                                />
+                              </Grid>
+                            </ListItem>
 
-        </Grid>
-      </Grid>
-      </Paper>
+                            <ListItem>
+                              <Grid item xs={12}>
+                                <Autocomplete
+                                  multiple
+                                  fullWidth
+                                  id="tags"
+                                  options={categories.map((option) => option.name)}
+                                  freeSolo
+                                  renderTags={(tag, getTagProps) => tag.map((option, index) => (
+                                    <>
+                                      <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+                                    </>
+                                  ))
+        }
+                                  onChange={(event) => onChangeTags(event)}
+                                  renderInput={(params) => (
+                                    <TextField {...params} variant="outlined" label="Categories" placeholder="Category" />
+                                  )}
+                                />
+                              </Grid>
+                            </ListItem>
+
+                            <ListItem>
+                              <Grid item xs={12}>
+                                <Autocomplete
+                                  id="next-bundles"
+                                  options={getNextBundles()}
+                                  getOptionLabel={(option) => option.name}
+                                  renderInput={(params) => <TextField {...params} label="Next Bundle" variant="outlined" />}
+                                />
+                              </Grid>
+                            </ListItem>
+
+                            <ListItem>
+                              <Grid item xs={12}>
+                                <Autocomplete
+                                  id="study-content"
+                                  options={getStudyContent()}
+                                  getOptionLabel={(option) => option.name}
+                                  renderInput={(params) => <TextField {...params} label="Study Content" variant="outlined" />}
+                                />
+                              </Grid>
+                            </ListItem>
+                          </Grid>
+                        </List>
+                      </Form>
+                    </Formik>
+                    <Button color="error" variant="contained" onClick={() => setDialogVisible(true)}>Discard</Button>
+                    <AlertDialog
+                      config={{
+                        title: 'Discard?',
+                        message: '',
+                        positiveButtonText: 'Discard',
+                        negativeButtonText: 'Cancel',
+                        onButtonPressed: setDialogVisible,
+                      }}
+                      visible={dialogVisible}
+                    />
+
+                  </Box>
+
+                </Grid>
+              </Grid>
+            </Paper>
+          </Card>
+        </Box>
       </Container>
     </>
   );
 };
 
-export default withStyles(styles) (BundleInfo);
+export default withStyles(styles)(BundleInfo);
